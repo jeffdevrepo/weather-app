418945469ba2b7a9b42e1b91fc67467252ccbba1
Jeffrey de Mesa <30891156+Jeff-coding@users.noreply.github.com>
2024-03-12 04:33:26 +0800

    Added environment variables.
    Added environments files for all-in-one storage of all constant variables.

---
+++ src/environments/environments.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  auth: {
+    domain: 'dev-v3m5husct5eq3s2n.us.auth0.com',
+    clientId: '1XUogCrR98Pwpzum2OrYf4T2mhUBC0NJ',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  }
+}
