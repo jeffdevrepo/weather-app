c06a68ad130ede71c1741e315da8fb16cc52fa5b
Jeffrey de Mesa <30891156+Jeff-coding@users.noreply.github.com>
2024-03-12 05:05:02 +0800

    Weather forecast page module
    Created default look and configuration for weather API.

---
+++ src/app/weather-forecast/modules/weather-forecast.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WeatherForecastRoutingModule } from './weather-forecast-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WeatherForecastRoutingModule
+  ]
+})
+export class WeatherForecastModule { }
